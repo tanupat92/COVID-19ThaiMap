9c6102d003b52fe4acada4c436f0175f57d7c851
Tanupat Boonchalermvichien <tanupat92@gmail.com>
2020-04-10 12:08:08 +0700

    add hide

--- ui.R
+++ ui.R
@@ -12,36 +12,38 @@ library(shinyWidgets)
 
 
 shinyUI(bootstrapPage(
-
-   theme = shinythemes:: shinytheme('simplex'),
-    
-   leaflet::leafletOutput('map', width = '100%', height = '100%'),
-   absolutePanel(top=10, right=10, id='controls',
-                 shinyWidgets::sliderTextInput('agerange', 'Age range:',
-                             choices = 0:100,
-                             from_min = NULL,
-                             from_max = NULL,
-                             to_min = NULL,
-                             to_max = NULL,
-                             selected = c(30,60)),
-                 dateRangeInput("daterange", "Select confirmed date:", start = NULL, end = NULL, min = NULL,
-                                max = NULL, format = "yyyy-mm-dd", startview = "month", weekstart = 0,
-                                language = "en", separator = " to ", width = NULL),
-                 shinyWidgets::awesomeCheckboxGroup('sex', 'Sex:', choices=c('Male','Female'),
-                                      selected= c('Male','Female'), inline =TRUE),
-                 shinyWidgets::switchInput(
-                   "allnation",
-                   label = "Nationality", onLabel = "All", offLabel = "None", value = TRUE),
-                 shinyWidgets::pickerInput(
-                   'nations', 'Nationality:', choices=NULL, multiple = TRUE, selected = NULL),
-                 uiOutput('update'),
-                 uiOutput('myweb'),
-                 tags$style(type = 'text/css',"html, body {width:100%;height:100%}
-                            #controls{backgroud-color:white;padding:30px;"),
-                 actionButton('showabout', 'About', icon = icon('question-circle'))
   
-                 )
-   
+  theme = shinythemes:: shinytheme('simplex'),
+  
+  leaflet::leafletOutput('map', width = '100%', height = '100%'),
+  absolutePanel(top=10, right=10, id='controls', clas = "panel panel-default", fixed = TRUE, draggable = FALSE,
+                HTML('<button data-toggle="collapse" data-target="#demo">Hide</button>'),
+                tags$div(id='demo', class="collapse in",
+                         shinyWidgets::sliderTextInput('agerange', 'Age range:',
+                                                       choices = 0:100,
+                                                       from_min = NULL,
+                                                       from_max = NULL,
+                                                       to_min = NULL,
+                                                       to_max = NULL,
+                                                       selected = c(0,100)),
+                         dateRangeInput("daterange", "Select confirmed date:", start = NULL, end = NULL, min = NULL,
+                                        max = NULL, format = "yyyy-mm-dd", startview = "month", weekstart = 0,
+                                        language = "en", separator = " to ", width = NULL),
+                         shinyWidgets::awesomeCheckboxGroup('sex', 'Sex:', choices=c('Male','Female'),
+                                                            selected= c('Male','Female'), inline =TRUE),
+                         shinyWidgets::switchInput(
+                           "allnation",
+                           label = "Nationality", onLabel = "All", offLabel = "None", value = TRUE),
+                         shinyWidgets::pickerInput(
+                           'nations', 'Nationality:', choices=NULL, multiple = TRUE, selected = NULL),
+                         uiOutput('update'),
+                         uiOutput('myweb'),
+                         tags$style(type = 'text/css',"html, body {width:100%;height:100%}
+                                    #controls{backgroud-color:white;padding:20px;"),
+                         actionButton('showabout', 'About', icon = icon('question-circle')))
+                
+                )
+  
 ))
 
 
